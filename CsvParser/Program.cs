820901fd3fa8bd649bc0e470b1466e2e8eac8c47
Neal Robben <neal.robben@cegeka.com>
2019-06-02 14:35:59 +0200

    Experiment with Target/ItemGroup

--- CsvParser/Program.cs
+++ CsvParser/Program.cs
@@ -8,18 +8,8 @@ namespace CsvParser
     {
         static void Main(string[] args)
         {
-			var csv = @"Year,Title,Production Studio
-						2008,Iron Man,Marvel Studios
-						2008,The Incredible Hulk,Marvel Studios
-						2008,Punisher: War Zone,Marvel Studios
-						2009,X-Men Origins: Wolverine,20th Century Fox
-						2010,Iron Man 2,Marvel Studios
-						2011,Thor,Marvel Studios
-						2011,X-Men: First Class,20th Century Fox
-						";
-			
-			var sr = new StringReader(csv);
-			var csvReader = new CsvReader(sr);
+			var reader = new StreamReader(new FileStream("Marvel.csv",FileMode.Open));
+			var csvReader = new CsvReader(reader);
 			
 			foreach (var line in csvReader.Lines)
 				Console.WriteLine(line.First(p => p.Key == "Title").Value);
