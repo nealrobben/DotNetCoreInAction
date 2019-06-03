d8d8682615186486f1dae6c0fb761aaee60e5c09
Neal Robben <neal.robben@gmail.com>
2019-06-03 22:33:08 +0200

    Added PartCommand class/table

--- CreateTablesTest/WidgetScmDataAccess/ScmContext.cs
+++ CreateTablesTest/WidgetScmDataAccess/ScmContext.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Linq;
 
@@ -9,14 +11,14 @@ namespace WidgetScmDataAccess
 		private DbConnection connection;
 		public IEnumerable<PartType> Parts { get; private set; }
 		public IEnumerable<InventoryItem> Inventory { get; private set; }
-		
+
 		public ScmContext(DbConnection conn)
 		{
 			connection = conn;
 			ReadParts();
 			ReadInventory();
 		}
-		
+
 		private void ReadParts()
 		{
 			using (var command = connection.CreateCommand())
@@ -58,5 +60,68 @@ namespace WidgetScmDataAccess
 					p.Id == item.PartTypeId);
 			}
 		}
-	}
+
+        public void CreatePartCommand(PartCommand partCommand)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = @"INSERT INTO PartCommand
+                                    (PartTypeId, Count, Command)
+                                    VALUES
+                                    (@partTypeId, @partCount, @command);
+                                    SELECT last_insert_rowid();";
+            AddParameter(command, "@partTypeId", partCommand.PartTypeId);
+            AddParameter(command, "@partCount", partCommand.PartCount);
+            AddParameter(command, "@command",
+            partCommand.Command.ToString());
+            long partCommandId = (long)command.ExecuteScalar();
+            partCommand.Id = (int)partCommandId;
+        }
+
+        private void AddParameter(DbCommand cmd, string name, object value)
+        {
+            var p = cmd.CreateParameter();
+            if (value == null)
+                throw new ArgumentNullException("value");
+            Type t = value.GetType();
+            if (t == typeof(int))
+                p.DbType = DbType.Int32;
+            else if (t == typeof(string))
+                p.DbType = DbType.String;
+            else if (t == typeof(DateTime))
+                p.DbType = DbType.DateTime;
+            else
+                throw new ArgumentException(
+                $"Unrecognized type: {t.ToString()}", "value");
+            p.Direction = ParameterDirection.Input;
+            p.ParameterName = name;
+            p.Value = value;
+            cmd.Parameters.Add(p);
+        }
+
+        public IEnumerable<PartCommand> GetPartCommands()
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = @"SELECT
+                                    Id, PartTypeId, Count, Command
+                                    FROM PartCommand
+                                    ORDER BY Id";
+            var reader = command.ExecuteReader();
+            var partCommands = new List<PartCommand>();
+            while (reader.Read())
+            {
+                var cmd = new PartCommand()
+                {
+                    Id = reader.GetInt32(0),
+                    PartTypeId = reader.GetInt32(1),
+                    PartCount = reader.GetInt32(2),
+                    Command = (PartCountOperation)Enum.Parse(
+                typeof(PartCountOperation),
+                reader.GetString(3))
+                };
+                cmd.Part = Parts.Single(p => p.Id == cmd.PartTypeId);
+                partCommands.Add(cmd);
+            }
+            return partCommands;
+        }
+    }
 }
