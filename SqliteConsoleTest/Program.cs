a7553ab2d93feda29ae45d8cc482f003d4d11421
Neal Robben <neal.robben@cegeka.com>
2019-06-03 12:02:49 +0200

    Experiment with Sqlite

---
+++ SqliteConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SqliteConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			using (var connection = new SqliteConnection("Data Source=:memory:"))
+			{
+				connection.Open();
+				
+				var command = new SqliteCommand("SELECT 1;", connection);
+				long result = (long)command.ExecuteScalar();
+				Console.WriteLine($"Command output: {result}");
+			}
+			
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
