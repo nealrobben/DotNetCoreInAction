c546e077a2e7c86fc1607b423ebe702f626e2fab
Neal Robben <neal.robben@cegeka.com>
2019-06-04 16:08:24 +0200

    Added MarkdownService

---
+++ MarkdownService/MdlController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DocAsCode.MarkdownLite;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MarkdownService
+{
+    [Route("/")]
+    public class MdlController : Controller
+    {
+        private readonly IMarkdownEngine engine;
+
+        public MdlController(IMarkdownEngine engine)
+        {
+            this.engine = engine;
+        }
+
+        [HttpPost]
+        public IActionResult Convert()
+        {
+            var reader = new StreamReader(Request.Body);
+            var markdown = reader.ReadToEnd();
+            var result = engine.Markup(markdown);
+            return Content(result);
+        }
+    }
+}
