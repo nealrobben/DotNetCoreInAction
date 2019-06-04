bf850b4660a9d74fabdd0adb15b1af7b038389de
Neal Robben <neal.robben@cegeka.com>
2019-06-04 16:01:04 +0200

    Added MarkdownLiteTest

---
+++ MarkdownLiteTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.DocAsCode.MarkdownLite;
+
+namespace MarkdownLiteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"
+                            Building Your First .NET Core Applications
+                            =======
+                            In this chapter, we will learn how to setup our development environment,
+                            create an application, and
+                            ";
+            var builder = new GfmEngineBuilder(new Options());
+            var engine = builder.CreateEngine(
+            new HtmlRenderer());
+            var result = engine.Markup(source);
+            Console.WriteLine(result);
+        }
+    }
+}
