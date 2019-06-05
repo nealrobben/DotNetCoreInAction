aafa9b990dfbd5ad75dacf7d27426154dec7da2d
Neal Robben <neal.robben@cegeka.com>
2019-06-05 09:09:01 +0200

    Added new project for localization

---
+++ ACController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exhaust Air Temp: " + TempControl.ExhaustAirTemp);
+        }
+    }
+}
---
+++ ACController/TempControl.cs
@@ -0,0 +1,9 @@
+﻿namespace ACController
+{
+    public class TempControl
+    {
+        public static double ExhaustAirTemp { get; internal set; }
+        public static double CoolantTemp { get; internal set; }
+        public static double OutsideAirTemp { get; internal set; }
+    }
+}
